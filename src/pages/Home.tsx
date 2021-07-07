c3cecbd59a3c6e9df5e7d2fb69308935e83d5cba
masterscode <ogbinakaemmanuel@gmail.com>
2021-07-07 09:03:11 +0100

    add project folders

---
+++ src/pages/Home.tsx
@@ -0,0 +1,8 @@
+import {Typography} from "@material-ui/core";
+import React from "react";
+
+const Home = ()=>{
+    return <Typography variant='h2'>home page</Typography>
+};
+
+export default Home;
