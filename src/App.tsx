a5ff09d7cc8d7c66eb3041c41af06b1dd2be4097
masterscode <ogbinakaemmanuel@gmail.com>
2021-07-07 15:05:49 +0100

    ..

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,25 @@
-import React, { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React, { useState } from "react";
+import Routes from "./routes";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = React.useState({});
+  const fetchData = async ()=>{
+    const response = await fetch("http://localhost:5000/actuator/beans");
+    const responseData = await response.json();
+    const {bean} = responseData.context.application;
+    return bean;
+  }
+  React.useEffect(()=> {
+    
+    setData(data => fetchData());
+  });
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
+    <section>
+      { }
+      <Routes />
+    </section>
+  );
 }
 
-export default App
+export default App;
